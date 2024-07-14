d24a5b004fec63133cd5535bea14f92cd2f800db
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-14 14:50:15 -0300

    feat: inclusão do componente macro form no componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
