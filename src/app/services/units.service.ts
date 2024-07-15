89cccc0a883558a08015b5faababd76c30be0645
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-14 22:47:15 -0300

    feat: implementando cache no serviço

--- src/app/services/units.service.ts
+++ src/app/services/units.service.ts
@@ -1,17 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap, tap } from 'rxjs';
 import { UnitResponse } from '../models/units-response.model';
 import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UnitsService {
+  private unitsCache$ = new BehaviorSubject<UnitResponse | null>(null);
 
   http = inject(HttpClient);
 
   getAll(): Observable<UnitResponse> {
-    return this.http.get<UnitResponse>(environment.apiURL);
+    if (this.unitsCache$.getValue() !== null) {
+      return this.unitsCache$ as Observable<UnitResponse>;
+    }
+    return this.http.get<UnitResponse>(environment.apiURL).pipe(
+      tap((data: UnitResponse) => this.unitsCache$.next(data)),
+      switchMap(() => this.unitsCache$ as Observable<UnitResponse>)
+    );
   }
 }
