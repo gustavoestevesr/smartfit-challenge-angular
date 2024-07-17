0bb12c9456d1147407efa1b1fa5b65d0bd48b1b2
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-16 23:21:37 -0300

    feat: função onClear, envio de eventos e filtro de unidades parcial

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,24 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { filter, map, take } from 'rxjs';
-import { UnitsService } from '../../services/units.service';
+import { take } from 'rxjs';
 import {
-  Location,
-  Schedule,
-  UnitResponse,
+  Location
 } from '../../models/units-response.model';
+import { UnitsService } from '../../services/units.service';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
 export class FormComponent {
   formGroup!: FormGroup;
-  readonly workoutPeriod: { key: string; valueFront: string; valueBack: string }[] = [
+  readonly weekDays = ['Dom.', 'Seg.', 'Ter.', 'Qua.', 'Qui.', 'Sex.', 'Sáb.'];
+  readonly workoutPeriod = [
     {
       key: 'Manhã',
       valueFront: '06:00 às 12:00',
@@ -38,35 +36,74 @@ export class FormComponent {
   results: Location[] = [];
   filteredResults: Location[] = [];
 
+  @Output() submitEvent = new EventEmitter<boolean>();
+  @Output() clearEvent = new EventEmitter<boolean>();
+
   private readonly formBuilder = inject(FormBuilder);
   private readonly unitsService = inject(UnitsService);
 
   constructor() {
     this.formGroup = this.formBuilder.group({
-      workoutPeriod: this.workoutPeriod[0].key,
+      workoutPeriod: this.workoutPeriod[0].valueBack,
       showClosedUnits: false,
     });
   }
 
-  onSubmit() {
+  extractOpenCloseHour(workoutPeriod: string){
+    const workoutPeriodSplitted = workoutPeriod.replaceAll('h', '').split(' ');
+    const openHour = Number(workoutPeriodSplitted[0]);
+    const closeHour = Number(workoutPeriodSplitted[2]);
+    return { openHour, closeHour };
+  }
+
+  filterUnits(opened: boolean, workoutPeriod: string) {
+    const weekDay = this.weekDays[new Date().getDay()];
+    const { openHour, closeHour } = this.extractOpenCloseHour(workoutPeriod);
+
+    console.log({
+      opened,
+      weekDay,
+      openHour,
+      closeHour,
+    });
+
     this.unitsService
       .getAll()
       .pipe(
         take(1),
-        map((data) => ({
-          locations: data.locations.filter((location) => {
-            return (
-              location.opened !== this.formGroup.get('showClosedUnits')?.value &&
-              location.schedules &&
-              location.schedules.some((schedule: Schedule) => {
-                return schedule.hour.includes(this.formGroup.get('workoutPeriod')?.value);
-              })
-            );
-          }),
-        }))
+        /*map((data: UnitsResponse) => {
+          if (data.locations) {
+            return data.locations.filter((location: Location) => {
+              if (location.schedules) {
+                return location.schedules.filter((schedule: Schedule) => {
+                  if (schedule.hour !== 'Fechada') {
+                    return schedule.
+                  }
+                });
+              }
+              return false;
+            });
+          }
+          return [];
+        })*/
       )
-      .subscribe((data) => {
-        this.results = data.locations;
+      .subscribe((data: Location[]) => {
+        this.filteredResults = data;
       });
+  }
+
+  onClear() {
+    this.formGroup.get('workoutPeriod')?.setValue(this.workoutPeriod[0].valueBack);
+    this.formGroup.get('showClosedUnits')?.setValue(false);
+
+    this.filteredResults = [];
+    this.clearEvent.emit(true);
+  }
+
+  onSubmit() {
+    const opened = this.formGroup.get('showClosedUnits')?.value;
+    const workoutPeriodForm = this.formGroup.get('workoutPeriod')?.value;
+    this.filterUnits(opened, workoutPeriodForm);
+    this.submitEvent.emit(true);
   }
 }
