f1d3d6224be36b8d2310995579eabd20d0e79c32
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-17 17:28:17 -0300

    feat: remoção de código e lógica de busca por horários

--- src/app/services/filter-units.service.ts
+++ src/app/services/filter-units.service.ts
@@ -1,35 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Location } from '../models/units-response.model';
-import { UnitsService } from './units.service';
-
-const weekDays = ['Dom.', 'Seg.', 'Ter.', 'Qua.', 'Qui.', 'Sex.', 'Sáb.'];
-const WORKOUT_HOURS = [
-  {
-    morning: {
-      start: 6,
-      end: 12,
-    },
-  },
-  {
-    afternoon: {
-      start: 6,
-      end: 12,
-    },
-  },
-  {
-    night: {
-      start: 18,
-      end: 23,
-    },
-  },
-];
+import { Location, Schedule } from '../models/units-response.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FilterUnitsService {
-  private weekDay = weekDays[new Date().getDay()];
-
   constructor() {}
 
   filterUnits(
@@ -38,19 +13,29 @@ export class FilterUnitsService {
     workoutPeriod: string,
   ) {
     let filteredList: Location[] = listOfUnits;
-    const { openHour, closeHour } = this.extractOpenCloseHour(workoutPeriod);
+    const { openHour: openHourFilter, closeHour: closeHourFilter } = this.extractOpenCloseHour(workoutPeriod);
 
     // Filter based on whether units should be shown if they are closed or not
     if (!showClosed) {
       filteredList = filteredList.filter((unit: Location) => {
-        if (unit.opened && unit.opened === true) {
-          return true
+        return unit.opened && unit.opened === true;
+      });
+    }
+
+    if (openHourFilter && closeHourFilter) {
+      filteredList = filteredList.filter((unit: Location) => {
+        if (unit.schedules) {
+          return unit.schedules.some((schedule: Schedule) => {
+            if (schedule.hour && schedule.hour.toString() !== 'Fechada') {
+              const { openHour, closeHour } = this.extractOpenCloseHour(schedule.hour);
+              return openHourFilter <= openHour && closeHourFilter >= closeHour;
+            }
+            return false;
+          });
         }
         return false;
       });
     }
-
-
     return filteredList;
   }
 
@@ -60,40 +45,4 @@ export class FilterUnitsService {
     const closeHour = Number(workoutPeriodSplitted[2]);
     return { openHour, closeHour };
   }
-
-  /*filterUnits(opened: boolean, workoutPeriod: string) {
-    const weekDay = this.weekDays[new Date().getDay()];
-    const { openHour, closeHour } = this.extractOpenCloseHour(workoutPeriod);
-
-    console.log({
-      opened,
-      weekDay,
-      openHour,
-      closeHour,
-    });
-
-    this.unitsService
-      .getAll()
-      .pipe(
-        take(1),
-        map((data: UnitsResponse) => {
-          if (data.locations) {
-            return data.locations.filter((location: Location) => {
-              if (location.schedules) {
-                return location.schedules.filter((schedule: Schedule) => {
-                  if (schedule.hour !== 'Fechada') {
-                    return schedule.
-                  }
-                });
-              }
-              return false;
-            });
-          }
-          return [];
-        })
-      )
-      .subscribe((data) => {
-        this.filteredResults = data.locations;
-      });
-  }*/
 }
