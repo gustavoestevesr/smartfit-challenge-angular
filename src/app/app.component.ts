f43f152d27733a17476a71fe28e876205bf6c411
gustavoesteves <gustavoestevesr@directon.com.br>
2024-07-16 23:20:56 -0300

    feat: adição footer ao app e ouvindo eventos do filho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { FooterComponent } from './components/footer/footer.component';
 import { FormComponent } from './components/form/form.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LegendComponent } from './components/legend/legend.component';
+import { ListUnitsComponent } from './components/list-units/list-units.component';
+import { Location } from './models/units-response.model';
+import { UnitsService } from './services/units.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FormComponent],
+  imports: [CommonModule, HeaderComponent, FormComponent, LegendComponent, ListUnitsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'smartfit-challenge-angular';
+  showList$ = new BehaviorSubject<boolean>(false);
+  listOfUnits: Location[] = [];
+
+  readonly unitsService = inject(UnitsService);
+
+  onSubmitEvent() {
+    this.listOfUnits = this.unitsService.getFilteredList();
+    this.showList$.next(true);
+  }
+
+  onClearEvent() {
+    this.showList$.next(false);
+  }
 }
